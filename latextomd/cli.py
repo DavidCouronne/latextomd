77ec437af797a3a4e0b4b46a60059b96f38b4759
David Couronné <couronne.david@gmail.com>
2020-01-18 11:23:33 +0100

    Update cli.py

--- latextomd/cli.py
+++ latextomd/cli.py
@@ -2,7 +2,14 @@ import sys
 
 
 def main():
-    print("hello world !!")
+    if len(sys.argv)==1:
+        print('You must enter a source file')
+    elif len(sys.argv)==2:
+        source_file = sys.argv[1]
+        export_file = source_file.replace('.tex','.md')
+    else:
+        source_file = sys.argv[1]
+        export_file = sys.argv[2]
 
 if __name__ == '__main__':
     main()
